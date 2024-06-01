bb05a895f80e76c16ef03d3f3616a5cfe0d6d513
stoneprogrammer <stoneprogrammer@gmail.com>
2024-06-01 12:16:59 -0600

    shadcn-ui init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ModeToggle } from "./components/mode-toggle";
+import { ThemeProvider } from "./components/theme-provider";
+
 export default function App() {
   return (
-    <div>
-      <h1 className="text-center text-9xl underline">Moonrakers</h1>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
+      <main className="flex h-screen items-center justify-center bg-gray-100 dark:bg-gray-900">
+        <h1 className="text-center text-9xl underline">Moonrakers</h1>
+        <ModeToggle />
+      </main>
+    </ThemeProvider>
   );
 }
